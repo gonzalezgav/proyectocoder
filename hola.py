61354aea513edef8ded19007c88d9f3176dd48a7
gonzalezgav <g88gonzalez@gmail.com>
2022-06-13 22:18:54 -0300

    segundo coder de la noche

--- hola.py
+++ hola.py
@@ -1,4 +1,6 @@
 
-print("hola mundo, el primer script que hago")
+#print("hola mundo, el primer script que hago ok")
+print("hola mundo, el primer script que hagk")
 
 # C:\Users\garumani\Downloads\presentacion_coder\python_coder\hola.py
+#nuevo comentario
